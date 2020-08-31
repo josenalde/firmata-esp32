0a6de61508d0b1fbbdfa36e8c1af022c1e0d22eb
josenaldeOliveira <josenalde@gmail.com>
2020-08-31 18:51:33 -0300

    Update testio1.rb

--- testioRB/testio1.rb
+++ testioRB/testio1.rb
@@ -1,14 +1,15 @@
 # #!/usr/bin/env ruby
-$:.unshift File.expand_path '../../lib', File.dirname(__FILE__)
-require'rubygems'
+#$:.unshift File.expand_path '../../lib', File.dirname(__FILE__)
+
+#require'rubygems'
 require'arduino_firmata'
 
-esp32 =ArduinoFirmata.connect "COM5"
-puts"firmata version #{esp32.version}"
+esp32 = ArduinoFirmata.connect "COM5"
+puts "firmata version #{esp32.version}"
 
 ## regist event
-esp32.on :sysexdo |command, data|
-    if command ==0x03
+esp32.on :sysex do |command, data|
+    if command == 0x03
         puts"command : #{command}"
         puts"data : #{data.inspect}"
         puts'------------------------------------'

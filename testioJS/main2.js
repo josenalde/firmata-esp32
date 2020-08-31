f27ab744c0b858a77e2872d85bfd8bd07cf933b8
josenaldeOliveira <josenalde@gmail.com>
2020-08-31 13:37:37 -0300

    Update main2.js

--- testioJS/main2.js
+++ testioJS/main2.js
@@ -1,12 +1,3 @@
-/*https://www.npmjs.com/package/arduino-firmata
-  fora de uso - 4 anos - 0.3.4
-
-  utilizar https://github.com/firmata/firmata.js?utm_source=recordnotfound.com
-  https://www.npmjs.com/package/firmata 2.2.0, 5 meses atrás
-  10.06.2020
-
-  npm install firmata
-  */
  /* by Josenalde Oliveira, 11.06.2020 ---- 
     sysex Commands for basic IO with ESP32 */ 
  const Firmata = require("firmata");
@@ -15,7 +6,7 @@
 
  const ADCREAD  = 0x02;
  
- var adcPinToRead = 32;
+ var adcPinToRead = 32; //feel free to change following adc1 channel pins (esp32). Adc2 will be added in near future.
  var rawValue, voltageValue;
  
   function analogRead() {
@@ -26,17 +17,17 @@
   function fhandler(data) {
     rawValue = Firmata.decode(data)[0];
     voltageValue = rawValue * (3.3/256);
-    console.log(voltageValue); 
+    console.log(voltageValue); // prints analog read value
   }
 
   esp32.on("ready", () => {
        console.log('esp32 is ready');
-       setInterval(analogRead, 100);
+       setInterval(analogRead, 100); //this interval must be adjusted-tuned according to your application
   });
         // framework -> esp32 (hw firmata.h host)
         //    argv[0] = 2 (pin), argv[1] = 2 (times), argv[2] = duration
         // esp32 (hw firmata.h host) -> framework
-        //    Firmata.sendSysex(command, argc, argv) usa sendValueAsTwo7bitBytes(bytev[i]);    
+        //    Firmata.sendSysex(command, argc, argv) calls sendValueAsTwo7bitBytes(bytev[i]);    
         /*
         * Split a 16-bit byte into two 7-bit values and write each value.
         * @param value The 16-bit value to be split and written separately.

1af2d73b1ef26602c963aafe417e77523052dc74
josenaldeOliveira <josenalde@gmail.com>
2020-09-24 21:34:34 -0300

    Add files via upload

---
+++ testioRB/keylistener.rb
@@ -0,0 +1,14 @@
+# adapted from https://stackoverflow.com/questions/946738/detect-key-press-non-blocking-w-o-getc-gets-in-ruby
+# post 9
+
+loop do
+    # a simple key listener for linux
+    system("stty raw -echo") # wait a keypress without enter
+    char = STDIN.read_nonblock(1) rescue nil
+    system("stty -raw echo")
+    break if /q/i =~ char # waits a 'q' keypress
+    # here goes your code
+    sleep 0.1 
+    puts 'test key listener linux'
+  end
+
